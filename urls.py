0b6be65c535b65a1eae8fe02feaef2485c23ffc6
mahamudh472 <mahadymahamudh472@gmail.com>
2024-12-14 21:18:32 +0600

    login form added

--- urls.py
+++ urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 
+app_name = 'accounts'
 urlpatterns = [
-    path('login/', views.user_login, name="login")
-]
+    path('login/', views.loginView, name="login"),
+    path('signup/', views.signup ,  name="signup")
+]   
--- views.py
+++ views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 
-def user_login(request):
-    return render(request, 'admin/accounts/login.html')
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log in the user after signup
+            return redirect('admin:index')  # Redirect to the admin index or any desired page
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form})
+
+
+def loginView(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return HttpResponse("Login successful")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', context={'form': form})
